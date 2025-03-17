b4864289b658d636d9ab26d241d20f992abe3a0c
yogeshsingh17 <singhyogesh492@gmail.com>
2025-03-17 16:28:52 +0530

    Added react query config

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const queryClient = new QueryClient();                // QueryClient is a object which is passed to client prop.
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <QueryClientProvider client={queryClient}>        
+                                                      {/*The client QueryClientProvider component makes sure 
+                                                      that react-queries are enables in our whole "app" 
+                                                      component. This component takes a client prop which 
+                                                      takes a QueryClient object.*/}
     <App />
-  </StrictMode>,
+  </QueryClientProvider>
 )
